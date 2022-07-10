d377f12d89c3b01bfa93de3ea53612950814df39
BenFestus <75408600+BenFestus@users.noreply.github.com>
2022-07-10 21:00:14 +0100

    Revert "Add files via upload"
    This reverts commit a176e3cc7a5a73463e8aa88db12559f15d42e0b2.

--- src/main/java/com/example/project/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.example.project;
-
-public class Utils {
-    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
-
-    public static final String EMAIL = "benjaminambrose99@gmail.com";
-    public static final String PASSWORD = "ieoiwkepwnspmcqg";
-}
